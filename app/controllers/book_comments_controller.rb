39809d89d55f99f817867a7ff23cef8cc068c802
EC2 Default User <ec2-user@ip-172-31-10-26.ec2.internal>
2020-11-07 06:06:32 +0000

    [Finish]bookers2-6

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,17 +3,17 @@ class BookCommentsController < ApplicationController
   before_action :ensure_correct_user, only: [:destroy]
   
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = BookComment.new(book_comment_params)
     comment.user_id = current_user.id
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.save
-    redirect_to book_path(book)
   end
   
   def destroy
-    BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+    comment = BookComment.find(params[:id])
+    comment.destroy
   end
   
   private
